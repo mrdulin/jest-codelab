ac5029a670e47cf3cefa67adc33c2192974e41e8
mrdulin <novaline.dulin@gmail.com>
2019-03-15 13:54:43 +0800

    improve automatic mocks

--- src/automatic-mocks/ts/__tests__/index.spec.ts
+++ src/automatic-mocks/ts/__tests__/index.spec.ts
@@ -1,24 +1,12 @@
 import utils from '../utils';
 
 describe('automatic mocks test suites', () => {
-  // const apple = require('../apple');
-  // describe('enableAutomock - Enables automatic mocking in the module loader.', () => {
-  //   const grape = require('../grape');
-  //   it('without enableAutomock, each module should be a real module through require', () => {
-  //     expect(banana()).toBe('banana');
-  //     expect(apple()).toBe('apple');
-  //     expect(grape()).toBe('grape');
-  //   });
-  //   // it('with enableAutomock', () => {
-  //   //   jest.enableAutomock();
-  //   //
-  //   //   const orange = require('../orange');
-  //   //
-  //   //   expect(orange()).toBeUndefined();
-  //   // });
-  // });
-
   it('if utils are mocked', () => {
     expect((utils.getJSON as jest.Mock).mock).toBeTruthy();
+  });
+
+  it('mocked implementation', () => {
+    (utils.getJSON as jest.Mock).mockReturnValue(123);
+    expect(utils.getJSON({ name: 'test' })).toBe(123);
   });
 });
