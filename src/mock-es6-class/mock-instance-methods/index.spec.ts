585493ed2b5c243106ea112c8f655633550dbe61
mrdulin <novaline.dulin@gmail.com>
2019-03-21 13:23:12 +0800

    add mock es6 class sample

---
+++ src/mock-es6-class/mock-instance-methods/index.spec.ts
@@ -0,0 +1,38 @@
+import LocationDAOImpl, { ILocationDAO, ILocation } from './index';
+
+jest.mock('./index', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      findById: jest.fn((id: number) => {
+        return { id, name: 'mock location' };
+      }),
+      findAllIds: jest.fn()
+    };
+  });
+});
+
+describe('mock es6 class', () => {
+  describe('mock instances method', () => {
+    describe('LocationDAOImpl', () => {
+      const locationDaoImpl: ILocationDAO = new LocationDAOImpl();
+      beforeEach(() => {
+        // not working
+        // (LocationDAOImpl as jest.Mock<LocationDAOImpl>).mockClear();
+
+        // not working
+        jest.clearAllMocks();
+      });
+
+      it('#findById', async () => {
+        const location: ILocation | undefined = await locationDaoImpl.findById(666);
+        const expectValue = { id: 666, name: 'mock location' };
+        expect(location).toEqual(expectValue);
+      });
+
+      it('clear findById mock', async () => {
+        const location: ILocation | undefined = await locationDaoImpl.findById(555);
+        console.log('location: ', location);
+      });
+    });
+  });
+});
