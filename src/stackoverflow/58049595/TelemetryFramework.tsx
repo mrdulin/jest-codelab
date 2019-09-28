2d0c37dbb2d8148159696aa863c8c1515e50fcb1
mrdulin <novaline.dulin@gmail.com>
2019-09-28 20:57:04 +0800

    add sample

---
+++ src/stackoverflow/58049595/TelemetryFramework.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TelemetryFramework extends Component {
+  public render() {
+    return <div>TelemetryFramework</div>;
+  }
+  protected telemetryInfo(...args: any[]) {
+    //
+  }
+  protected getconfidentialData() {
+    //
+  }
+}
+
+export default TelemetryFramework;
