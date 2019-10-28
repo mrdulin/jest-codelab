7a1ae6b25c2fc07f62c210624115ba1d3dae85f9
mrdulin <novaline.dulin@gmail.com>
2019-10-28 13:50:42 +0800

    add Keywords performance report and Keywordless Query report

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   // testEnvironment: 'node'
   testEnvironment: 'enzyme',
+  setupFiles: ['/Users/ldu020/workspace/github.com/mrdulin/jest-codelab/src/stackoverflow/58585527/window.setup.js'],
   setupFilesAfterEnv: ['jest-enzyme'],
   testEnvironmentOptions: {
     enzymeAdapter: 'react16'
