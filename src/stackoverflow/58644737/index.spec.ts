2463b5789a8ea66947c1b27fc1ec08d09e78c097
mrdulin <novaline.dulin@gmail.com>
2019-11-01 01:10:06 +0800

    improve sample

--- src/stackoverflow/58644737/index.spec.ts
+++ src/stackoverflow/58644737/index.spec.ts
@@ -1,9 +1,25 @@
-import { main } from './';
+import { Component } from './';
+
+const cmp = new Component();
 
 describe('main', () => {
-  test('should mock FileReader', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+  test('should test handle file upload correctly', () => {
+    const mFile = new File(['go'], 'go.pdf');
+    const mEvent = { target: { files: [mFile] } };
     const readAsBinaryStringSpy = jest.spyOn(FileReader.prototype, 'readAsBinaryString');
-    main();
-    expect(readAsBinaryStringSpy).toBeCalledWith(new Blob());
+    const btoaSpy = jest.spyOn(window, 'btoa');
+    const reader = cmp.handleFileUpload(mEvent);
+    expect(reader).toBeInstanceOf(FileReader);
+    if (reader.onload) {
+      Object.defineProperty(reader, 'result', { value: 'gogo' });
+      const mOnloadEvent = {} as any;
+      reader.onload(mOnloadEvent);
+      expect(btoaSpy).toBeCalledWith('gogo');
+      expect(cmp.object.image).toBe(btoa('gogo'));
+    }
+    expect(readAsBinaryStringSpy).toBeCalledWith(mFile);
   });
 });
--- src/stackoverflow/58644737/index.ts
+++ src/stackoverflow/58644737/index.ts
@@ -1,5 +1,18 @@
-export function main() {
-  const fr = new FileReader();
-  const blob = new Blob();
-  fr.readAsBinaryString(blob);
+export class Component {
+  object = {
+    image: ''
+  };
+  handleFileUpload(event) {
+    let reader = new FileReader();
+    let file = event.target.files[0];
+
+    reader.readAsBinaryString(file);
+
+    reader.onload = () => {
+      let base64String = btoa(reader.result as string);
+      this.object.image = base64String;
+    };
+
+    return reader;
+  }
 }
