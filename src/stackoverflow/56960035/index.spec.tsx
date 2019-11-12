d82bfea029195f694a6f7517d2e97f4f5cfbfc3d
mrdulin <novaline.dulin@gmail.com>
2019-11-12 11:44:02 +0800

    improve sample

--- src/stackoverflow/56960035/index.spec.tsx
+++ src/stackoverflow/56960035/index.spec.tsx
@@ -5,7 +5,7 @@ import { mount } from 'enzyme';
 describe('Avatar', () => {
   it('should handle error', () => {
     const mProps = {
-      imageSrc: '123.com',
+      imageSrc: 'http://123.com/avatar.png',
       imageAlt: 'alt',
       imageWidth: 123,
       imageHeight: 456
@@ -17,6 +17,7 @@ describe('Avatar', () => {
       style: { width: mProps.imageWidth, height: mProps.imageHeight },
       onError: expect.any(Function)
     });
+    expect((wrapper.getDOMNode() as HTMLImageElement).src).toBe(mProps.imageSrc);
     const mEvent = { target: wrapper.getDOMNode() } as any;
     wrapper.find('img').prop('onError')!(mEvent);
     expect(mEvent.target.src).toBe('http://localhost/static/images/placeholder/avatar.png');
