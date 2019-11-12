1c427c4be2e22b3f810d63c44bff6081251a9176
mrdulin <novaline.dulin@gmail.com>
2019-11-12 10:19:04 +0800

    add sample

---
+++ src/stackoverflow/58807874/index.js
@@ -0,0 +1,25 @@
+const fetchData = async (product, numOfProducts) => {
+  const res = await fetch('https://github.com/mrdulin');
+  const data = await res.json();
+  return data;
+};
+
+// user input for products looks like 'butter 2'
+// selecting Breaskstone's butter (from mockData below)
+export const addToShoppingList = async (product, numOfProducts = 10, getProducts = fetchData) => {
+  const shoppingList: any[] = [];
+
+  const item = product.slice(0, -2); // i.g. 'butter'
+  const i = +product.split(' ').pop() - 1; // i.g. '2' which becomes 1, the index number in array
+  const data = await getProducts(item, numOfProducts);
+
+  // i, which is the index, is used to grab specific item from list of products
+  let products = data.items[i].productInfo.name;
+  let manufacturers = data.items[i].productInfo.manufacturer;
+
+  const chosenItem = `${products} from ${manufacturers}`;
+
+  shoppingList.push(chosenItem);
+
+  return shoppingList;
+};
---
+++ src/stackoverflow/58807874/index.spec.js
@@ -0,0 +1,33 @@
+import { addToShoppingList } from './';
+
+const mockData = {
+  items: [
+    {
+      productInfo: {
+        name: 'Butter',
+        manufacturer: 'Land O Lakes'
+      }
+    },
+    {
+      productInfo: {
+        name: 'Butter',
+        manufacturer: 'Breakstone'
+      }
+    },
+    {
+      productInfo: {
+        name: 'Butter',
+        manufacturer: 'Kerrygold'
+      }
+    }
+  ]
+};
+
+describe('addToShoppingList', () => {
+  it('should call fetch with correct arguments', async () => {
+    const mockProducts = jest.fn().mockReturnValue(mockData);
+    const actualValue = await addToShoppingList('product 1', 100, mockProducts);
+    expect(actualValue).toEqual(['Butter from Land O Lakes']);
+    expect(mockProducts).toBeCalledWith('product', 100);
+  });
+});
