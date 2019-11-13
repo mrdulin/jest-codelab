531476dadd1ed9864fdfe806b9462cc20fe3c7fa
mrdulin <novaline.dulin@gmail.com>
2019-11-13 12:34:30 +0800

    add sample

---
+++ src/stackoverflow/58820204/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AutoComplete extends Component<any> {
+  render() {
+    return (
+      <div>
+        <ul className={this.props.className} onClick={this.props.onClick}>
+          {this.props.match.map(item => {
+            return <li key={item.id}>{item.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default AutoComplete;
---
+++ src/stackoverflow/58820204/index.spec.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AutoComplete from './index';
+
+describe('AutoComplete', () => {
+  it('render list', () => {
+    const handleClick = jest.fn();
+    const mockList = [
+      { id: 1011322, name: 'Druig' },
+      { id: 1009284, name: 'Dum Dum Dugan' },
+      { id: 101078, name: 'Eddie Brock' }
+    ];
+    const div = document.createElement('div');
+    ReactDOM.render(<AutoComplete match={mockList} className={'autocomplete-items'} onClick={handleClick} />, div);
+    expect(div.querySelectorAll('.autocomplete-items li').length).toBe(3);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
