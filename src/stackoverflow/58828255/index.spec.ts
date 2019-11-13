2b4657db35fe58279ce34538951ac6e9fb2584e2
mrdulin <novaline.dulin@gmail.com>
2019-11-13 12:43:35 +0800

    add sample

---
+++ src/stackoverflow/58828255/index.spec.ts
@@ -0,0 +1,18 @@
+jest.unmock('axios');
+
+const axios = require('axios');
+
+describe('a request to the API POST /order', () => {
+  test('with a correct body - 1', () => {
+    return axios.post('http://localhost:3000/order', {}).catch(function(error) {
+      expect(1 + 4).toBe(3);
+    });
+  });
+
+  test('with a correct body - 2', done => {
+    axios.post('http://localhost:3000/order', {}).catch(function(error) {
+      expect(1 + 4).toBe(3);
+      done();
+    });
+  });
+});
