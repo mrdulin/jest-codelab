d17970bb2d06e747f11caf814ce574c6df1c3dba
mrdulin <novaline.dulin@gmail.com>
2019-12-10 20:11:41 +0800

    add sample

--- src/stackoverflow/59266532/ScriptTag.test.ts
+++ src/stackoverflow/59266532/ScriptTag.test.ts
@@ -5,5 +5,6 @@ describe('ScriptTag', () => {
     jest.spyOn(ScriptTag.prototype, 'getFileName').mockImplementation(() => 'scriptTag.js');
     const scriptTag: ScriptTag = new ScriptTag('subdomain.shopify.com', 'generalTokenValue', '../path/to/file.js');
     expect(scriptTag.localFileName).toEqual('scriptTag.js');
+    expect(scriptTag.getFileName).toBeCalled();
   });
 });
